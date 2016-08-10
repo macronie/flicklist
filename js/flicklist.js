2b238697cec2b1ca2f5abc978f4e785a9dc0674c
Poornima Vijayan <sudincn@gmail.com>
2016-08-10 18:44:28 +0000

    finish FlickList 1 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO" // TODO 0 put your api key here
+  token: "2f6ee448a14bbfbde0ae353360e6e3a0" // TODO 0 put your api key here
 }
 
 
@@ -29,9 +29,11 @@ function discoverMovies(callback) {
 			
 			// TODO 2
 			// update the model, setting its .browseItems property equal to the movies we recieved in the response
-			
+		
+			model.browseItems = response.results;
 			// invoke the callback function that was passed in. 
 			callback();
+			
 		}
 	});
   
@@ -45,21 +47,45 @@ function render() {
   // TODO 7
   // clear everything from both lists
   
+  	$( "#section-watchlist ul" ).empty();
+  	
+  	$( "#section-browse ul" ).empty();
+  
   // TODO 6
   // for each movie on the user's watchlist, insert a list item into the <ul> in the watchlist section
+    
+ 
+       model.watchlistItems.forEach(function(movie) {
+		
+		$("#section-watchlist ul").append("<br><li>" + movie + "</li><br>");
+		
+  });
   
   // for each movie on the current browse list, 
-  model.browseItems.forEach(function(movie) {
+		 
+		model.browseItems.forEach(function(movie) {
+		
 		// TODO 3
 		// insert a list item into the <ul> in the browse section
+		//$("#essay p")
+
+		$("#section-browse ul").append("<br><li>" + movie.title + "</li><br>");
 		
 		// TODO 4
 		// the list item should include a button that says "Add to Watchlist"
-		
+		$("#section-browse ul").append("<button id ="+ movie.title+"type='button'>Add to Watchlist</button><br>");
+  });
 		// TODO 5
 		// when the button is clicked, this movie should be added to the model's watchlist and render() should be called again
-  });
-  
+      $( "button" ).click(function() {
+       console.log("You clicked my button!");
+       console.log(this.id);
+       model.watchlistItems.push(this.id);
+       render();
+      });
+      
+
+
 }
 
 
